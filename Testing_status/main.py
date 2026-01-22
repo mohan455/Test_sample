640294e2a81213b4f2843bdaedbac5c831850566
mohan kuruba <mahimohan28k@gmail.com>
2026-01-22 17:44:23 +0530

    changes in the branch

--- Testing_status/main.py
+++ Testing_status/main.py
@@ -1,1 +1,15 @@
+import math
+import random
 print(abs(28.5689)) # output: the positive value of the given value
+print(math.floor(105.70))
+print(math.ceil(105.50))
+print("-----------")
+print(round(28.446,2))
+print(random.seed(5))
+print(random.random())
+print(random.uniform(1,5))
+print("----------")
+print(math.sin(3))
+print(math.sin(math.pi/2))
+print(math.cos(1))
+print(math.tan(math.pi/2))
